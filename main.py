e0000c760ef2aa11cbd15b617a92bead272dbd85
Deathgrip44 <vols9187@yahoo.com>
2023-06-18 15:21:05 -0500

    updated
    reworked main code and added a few more things to requirements in hopes it all works together

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 #Imported libraries
 
 import speech_recognition as sr
+from gtts import gTTS
+import os
+import datetime
+import playsound 
+import wikipedia
+import pyaudio
 
 
+#get mic audio
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+                print("Exception " + str(e))
+    return said 
 
-r = sr.Recognizer()
+#speaks converted text as audio
+def speak(text):
+    tts = gTTS(text=text, lang='en')
+    filename= "voice.mp3"
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+    tts.save(filename)
+    playsound.playsound(filename)
 
-#Gets the default microphone
-with sr.Microphone() as source:
-    #listens for voice using the microphone
-    audio = r.listen(source)
-
-    text = r.recognize_google(audio)
-
-    print(text)
+#test number 1
+text = get_audio()
+speak(text)
